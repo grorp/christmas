7fbc1a83e0223187f7aaf38cfc745acb53fb51df
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-12-08 20:44:24 +0100

    Also replace uppercase chars created by CSS

--- emojis.js
+++ emojis.js
@@ -1,9 +1,19 @@
 const treeWalker = document.createTreeWalker(document.body);
 while (treeWalker.nextNode()) {
 	const node = treeWalker.currentNode;
+
 	if (node.nodeType === Node.TEXT_NODE) {
 		node.nodeValue = node.nodeValue.replaceAll("A", "🎄");
 		node.nodeValue = node.nodeValue.replaceAll("O", "🍪");
 		node.nodeValue = node.nodeValue.replaceAll("I", "🕯️");
+
+		if (
+			window.getComputedStyle(node.parentNode).textTransform ===
+			"uppercase"
+		) {
+			node.nodeValue = node.nodeValue.replaceAll("a", "🎄");
+			node.nodeValue = node.nodeValue.replaceAll("o", "🍪");
+			node.nodeValue = node.nodeValue.replaceAll("i", "🕯️");
+		}
 	}
 }
